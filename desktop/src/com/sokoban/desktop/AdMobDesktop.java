62745a149c3f6673ec93586922b9ee210fd72071
dalejandroAguilar <37708907+dalejandroAguilar@users.noreply.github.com>
2018-09-25 13:28:32 -0500

    ads support

---
+++ desktop/src/com/sokoban/desktop/AdMobDesktop.java
@@ -0,0 +1,15 @@
+package com.sokoban.desktop;
+
+import com.sokoban.AdHandler;
+
+public class AdMobDesktop implements AdHandler {
+    @Override
+    public void showAds(boolean show) {
+
+    }
+
+    @Override
+    public float getHeightBanner() {
+        return 0;
+    }
+}
--- desktop/src/com/sokoban/desktop/DesktopLauncher.java
+++ desktop/src/com/sokoban/desktop/DesktopLauncher.java
@@ -2,11 +2,22 @@ package com.sokoban.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sokoban.AdHandler;
 import com.sokoban.MainGame;
 
-public class DesktopLauncher {
+public class DesktopLauncher implements AdHandler{
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainGame(), config);
+		new LwjglApplication(new MainGame(new AdMobDesktop()), config);
+	}
+
+	@Override
+	public void showAds(boolean show) {
+
+	}
+
+	@Override
+	public float getHeightBanner() {
+		return 0;
 	}
 }
