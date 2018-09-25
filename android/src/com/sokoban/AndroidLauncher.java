4d4656a8c5e4b500232d6e55318ac5728ff7a950
dalejandroAguilar <37708907+dalejandroAguilar@users.noreply.github.com>
2018-09-25 13:27:35 -0500

    new resources

--- android/src/com/sokoban/AndroidLauncher.java
+++ android/src/com/sokoban/AndroidLauncher.java
@@ -1,16 +1,93 @@
 package com.sokoban;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.sokoban.MainGame;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainGame(), config);
-	}
+public class AndroidLauncher extends AndroidApplication implements AdHandler {
+    private static final String TAG = "AndroidLauncher";
+    private final int SHOW_ADS = 1;
+    private final int HIDE_ADS = 0;
+    protected AdView adView;
+
+    Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_ADS:
+                    adView.setVisibility(View.VISIBLE);
+                    break;
+                case HIDE_ADS:
+                    adView.setVisibility(View.GONE);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RelativeLayout layout = new RelativeLayout(this);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        View gameView = initializeForView(new MainGame(this), config);
+        layout.addView(gameView);
+
+
+        adView = new AdView(this);
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                int visiblity = adView.getVisibility();
+                adView.setVisibility(AdView.GONE);
+                adView.setVisibility(visiblity);
+                Log.i(TAG, "Ad Loaded...");
+            }
+        });
+
+        adView.setAdSize(AdSize.SMART_BANNER);
+        //adView.setAdUnitId("ca-app-pub-5828894093809520/7263784026");
+//ca-app-pub-3940256099942544/6300978111
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        AdRequest.Builder builder = new AdRequest.Builder();
+
+        builder.addTestDevice("1E8FF7CB903FAB02597A10D98ECCC2AF");
+
+        RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        layout.addView(adView, adParams);
+        adView.loadAd(builder.build());
+        setContentView(layout);
+        //initialize(new MainGame(), config);
+    }
+
+    @Override
+    public void showAds(boolean show) {
+        handler.sendEmptyMessage(show ? SHOW_ADS : HIDE_ADS);
+    }
+
+    @Override
+    public float getHeightBanner() {
+        float heightResolution = Gdx.graphics.getHeight();
+        if (heightResolution <= 400)
+            return 32;
+        if (heightResolution < 720)
+            return 50;
+        return 90;
+    }
 }
